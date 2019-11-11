106265a70efd9f324c5a59ca42c0743f9f2fcbbe
jonathan.schneider <lennart.schneider@rwth-aachen.de>
2019-11-11 13:18:57 +0100

    Centroid initializing, input parser

--- k-fortran.f90
+++ k-fortran.f90
@@ -2,7 +2,7 @@ module lib
     implicit None
 
 contains
-    subroutine get_arguments (iterations, filename_input, n_samples, n_clusters, n_features)
+    subroutine get_arguments(iterations, filename_input, n_samples, n_clusters, n_features)
         character(len = :), allocatable, intent(out) :: filename_input
         integer, intent(out) :: iterations, n_samples, n_features, n_clusters
         character(len = :), allocatable :: iterations_char, clusters_char, samples_char, features_char
@@ -39,6 +39,46 @@ contains
         read(features_char, *) n_features
     end subroutine get_arguments
 
+    subroutine read_samples(filename_input, samples, assignments, n_samples, n_features)
+        character(len = :), allocatable, intent(in) :: filename_input
+        integer, intent(in) :: n_samples, n_features
+        real, dimension(:, :), allocatable, intent(out) :: samples
+        integer, dimension(:), allocatable, intent(out) :: assignments
+        integer :: current_line
+
+        allocate(samples(n_samples, n_features))
+        allocate(assignments(n_samples))
+
+        open(unit = 11, file = filename_input, status = "old")
+        do current_line = 1, n_samples
+            read(11, *) samples(current_line, :)
+        end do
+        close(11)
+    end subroutine read_samples
+
+    subroutine initialize_centroids(samples, centroids, n_samples, n_clusters, n_features)
+        integer, intent(in) :: n_samples, n_clusters, n_features
+        real, dimension(:, :), allocatable, intent(in) :: samples
+        real, dimension(:, :), allocatable, intent(out) :: centroids
+        real, dimension(n_features) :: mini, maxi
+        integer :: current_centroid, current_feature
+        real :: random_num
+
+        allocate(centroids(n_clusters, n_features))
+
+        do current_feature = 1, n_features
+            mini(current_feature) = minval(samples(:, current_feature))
+            maxi(current_feature) = maxval(samples(:, current_feature))
+        end do
+
+        do current_centroid = 1, n_clusters
+            do current_feature = 1, n_features
+                call random_number(random_num)
+                centroids(current_centroid, current_feature) = &
+                    mini(current_feature) + (maxi(current_feature) + 1 - mini(current_feature)) * random_num
+            end do
+        end do
+    end subroutine initialize_centroids
 end module lib
 
 program main
@@ -49,9 +89,17 @@ program main
     real :: start, finish
     integer :: iterations, n_samples, n_features, n_clusters
     character(len = :), allocatable :: filename_input
-    real, dimension(:, :), allocatable :: points, centroids
+    real, dimension(:, :), allocatable :: samples, centroids
     integer, dimension(:), allocatable :: assignments
 
+    ! Parse the arguments into the variables
     call get_arguments(iterations, filename_input, n_samples, n_clusters, n_features)
+    ! Read the input file into the samples array and initialize the assignments array
+    call read_samples(filename_input, samples, assignments, n_samples, n_features)
+
+    print *, "Performing k-means clustering on ", filename_input, " with ",&
+        iterations, " iteration(s)"
+
+    call initialize_centroids(samples, centroids, n_samples, n_clusters, n_features)
 
 end program main
